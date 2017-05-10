dbae3d401d3ab0257eaf6300514fc0653496afb8
linkjr <linkjr@sina.com>
2017-05-11 00:49:11 +0800

    Singleton design pattern.

---
+++ DesignPattern/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object obj = new object();
+
+        public Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    lock (obj)
+                        if (_instance == null)
+                            _instance = new Singleton();
+                return _instance;
+            }
+        }
+    }
+}
