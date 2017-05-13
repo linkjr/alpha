ac4cca593f29d7ead8908912f4655ba54841f78c
linkjr <linkjr@sina.com>
2017-05-14 00:36:32 +0800

    新增实例。

---
+++ Alpha.ConsoleApp/Instance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha.ConsoleApp
+{
+    public class Instance
+    {
+        public static IEnumerable<Type> FindInstancesByInterface<IType>()
+        {
+            var instances = from m in Assembly.GetExecutingAssembly().GetTypes()
+                        where m.GetInterfaces().Contains(typeof(IType))
+                        select m;
+            return instances;
+        }
+    }
+}
